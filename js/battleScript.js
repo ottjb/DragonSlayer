b3aa49584f8edf3e8a8a2c96438552ca22740f32
ottjb <ottjb@usca.edu>
2023-11-20 02:53:18 -0500

    Rogue Attack Working!!

--- js/battleScript.js
+++ js/battleScript.js
@@ -1,6 +1,7 @@
 /* 
 Todo:
 Battle Script
+CREATE FUNCTION THAT DOES THE DAMAGE FOR FUNCTIONS (WILL MAKE EASIER TO FIGURE OUT IF BATTLE ENDS, AND SHORTENS CODE)
 Moves for all characters
 Add enemy moves
 Add stat block bottom left
@@ -9,11 +10,14 @@ Add stat block bottom left
 /*
 Contents:
 Classes
-Character
-Putting Characters on screen
-Buttons
+Enemies
+Status Effects
+Character Constructor
 Test Characters
+Populate Screen
+Buttons
 Battle
+Functions Ran on Build
 Utility
 */
 
@@ -88,25 +92,55 @@ $(document).ready(function () {
       bonusDodge: 0,
       moveset: [
         {
-          name: attack,
+          name: "Lethal Backstab",
           effect: function () {
-            console.log("attack");
+            if (
+              genRandomNumber(1, 100) >
+              enemy.charClass.baseDodge + enemy.charClass.bonusDodge
+            ) {
+              var damage =
+                10 +
+                Math.round(
+                  (player.charClass.baseAttack + player.charClass.bonusAttack) *
+                    1.25
+                );
+              if (enemy.charClass.currentHP - damage < 0) {
+                enemy.charClass.currentHP = 0;
+              } else {
+                enemy.charClass.currentHP -= damage;
+              }
+              addToBattleLog(
+                `${enemy.name} took ${damage} damage from ${player.name}'s Lethal Backstab!`
+              );
+              if (genRandomNumber(1, 100) > 10) {
+                console.log("bleed", currentStatuses);
+                if (!checkStatus(enemy, "bleed")) {
+                  currentStatuses.push(["bleed", 3, enemy]);
+                  addToBattleLog(`${enemy.name} is now bleeding!`);
+                }
+              }
+            } else {
+              addToBattleLog(
+                `${enemy.name} dodged ${player.name}'s Lethal Backstab!`
+              );
+            }
+            updateHP();
           },
         },
         {
-          name: defend,
+          name: "Acrobatic Evasion",
           effect: function () {
             console.log("defend");
           },
         },
         {
-          name: heal,
+          name: "Quick Patch-Up",
           effect: function () {
             console.log("heal");
           },
         },
         {
-          name: special,
+          name: "Shadow Step",
           effect: function () {
             console.log("special");
           },
@@ -191,25 +225,144 @@ $(document).ready(function () {
     },
   ];
 
+  /////////////
+  // Enemies //
+  /////////////
+
+  enemies = [
+    // Bandit
+    {
+      name: "bandit",
+      sprites: "img/ranger/front/white.png",
+      baseMaxHP: 150,
+      currentHP: 150,
+      baseAttack: 20,
+      baseDefense: 10,
+      baseSpeed: 50,
+      baseDodge: 25,
+      bonusAttack: 0,
+      bonusDefense: 0,
+      bonusSpeed: 0,
+      bonusDodge: 0,
+      moveset: [
+        {
+          name: attack,
+          effect: function () {},
+        },
+        { name: defend, effect: function () {} },
+      ],
+    },
+
+    // Goblin
+    {
+      name: "goblin",
+      sprites: {},
+      baseMaxHP: 150,
+      currentHP: 150,
+      baseAttack: 20,
+      baseDefense: 10,
+      baseSpeed: 50,
+      baseDodge: 0,
+      bonusAttack: 0,
+      bonusDefense: 0,
+      bonusSpeed: 0,
+      bonusDodge: 0,
+      moveset: [
+        {
+          name: attack,
+          effect: function () {},
+        },
+        { name: defend, effect: function () {} },
+      ],
+    },
+
+    // Undead
+    {
+      name: "undead",
+      sprites: {},
+      baseMaxHP: 150,
+      currentHP: 150,
+      baseAttack: 20,
+      baseDefense: 10,
+      baseSpeed: 50,
+      baseDodge: 0,
+      bonusAttack: 0,
+      bonusDefense: 0,
+      bonusSpeed: 0,
+      bonusDodge: 0,
+      moveset: [
+        {
+          name: attack,
+          effect: function () {},
+        },
+        { name: defend, effect: function () {} },
+      ],
+    },
+
+    // Dragon
+    {
+      name: "dragon",
+      sprites: {},
+      baseMaxHP: 150,
+      currentHP: 150,
+      baseAttack: 20,
+      baseDefense: 10,
+      baseSpeed: 50,
+      baseDodge: 0,
+      bonusAttack: 0,
+      bonusDefense: 0,
+      bonusSpeed: 0,
+      bonusDodge: 0,
+      moveset: [
+        {
+          name: attack,
+          effect: function () {},
+        },
+        { name: defend, effect: function () {} },
+      ],
+    },
+  ];
+
   ////////////////////
   // Status effects //
   ////////////////////
 
   var statuses = [
     {
+      name: "bleed",
+      effect: function (character) {
+        var damage = Math.round(
+          character.charClass.baseMaxHP * 0.05 +
+            (character.charClass.baseMaxHP - character.charClass.currentHP) *
+              0.05
+        );
+        if (character.charClass.currentHP - damage < 0) {
+          character.charClass.currentHP = 0;
+        } else {
+          character.charClass.currentHP -= damage;
+        }
+        addToBattleLog(`${character.name} took ${damage} damage from Bleed!`);
+        updateHP();
+      },
+    },
+    {
       name: "burn",
       effect: function (character) {
         var damage = Math.round(character.charClass.baseMaxHP * 0.1);
-        character.charClass.currentHP -= damage;
+        if (character.charClass.currentHP - damage < 0) {
+          character.charClass.currentHP = 0;
+        } else {
+          character.charClass.currentHP -= damage;
+        }
         addToBattleLog(`${character.name} took ${damage} damage from Burn!`);
         updateHP();
       },
     },
   ];
 
-  ///////////////
-  // Character //
-  ///////////////
+  ///////////////////////////
+  // Character Constructor //
+  ///////////////////////////
 
   // This constructor uses a custom sprite
   function Character(name, charClass, sprite) {
@@ -217,6 +370,17 @@ $(document).ready(function () {
     this.charClass = charClass;
     this.sprite = sprite;
   }
+
+  /////////////////////
+  // Test Characters //
+  /////////////////////
+
+  const player = new Character(
+    "Cat Rogue",
+    characters[1],
+    characters[1].sprites.back.base
+  );
+  const enemy = new Character("Bad Guy", enemies[0], enemies[0].sprites);
 
   /////////////////////
   // Populate screen //
@@ -231,7 +395,6 @@ $(document).ready(function () {
     $(`#${position}MaxHP`).html(maxHp);
     updateHPBar(position, hp, maxHp);
     $(`#${position}Sprite`).attr("src", character.sprite);
-    console.log(character.sprite, "Sprite loaded");
   }
 
   // Updates the HP bar
@@ -240,61 +403,41 @@ $(document).ready(function () {
     $(`#${position}HPBar`).attr("max", maxHp);
   }
 
-  // Updates the stat block
-  function updateStatBlock(atk, def, spd, dodge) {
-    //stopped here, getting stat block to update when called in initializeBattle
-    console.log("Updating stat block");
-    $("#playerAtkSt").html(atk);
-    $("#playerDefSt").html(def);
-    $("#playerSpdSt").html(spd);
-    $("#playerDodgeSt").html(dodge);
-  }
-
   /////////////
   // Buttons //
   /////////////
 
   // Attack button
+  $("#attack").text(player.charClass.moveset[0].name);
   $("#attack").click(function () {
     initializeBattle(0);
   });
 
   // Defend button
+  $("#defend").text(player.charClass.moveset[1].name);
   $("#defend").click(function () {
     initializeBattle(1);
   });
 
   // Heal button
+  $("#heal").text(player.charClass.moveset[2].name);
   $("#heal").click(function () {
     initializeBattle(2);
   });
 
   // Special button
+  $("#special").text(player.charClass.moveset[3].name);
   $("#special").click(function () {
     initializeBattle(3);
   });
-
-  /////////////////////
-  // Test characters //
-  /////////////////////
-
-  const player = new Character(
-    "Cat Rogue",
-    characters[1],
-    characters[1].sprites.back.base
-  );
-  const enemy = new Character(
-    "Dragoon",
-    characters[2],
-    characters[2].sprites.front.base
-  );
 
   ////////////
   // Battle //
   ////////////
 
-  var currentStatuses = [["burn", 3, enemy]]; // [[status, turns left, target]
+  var currentStatuses = []; // [["status", turns left, target]]
   function initializeBattle(playerAction) {
+    disableButtons();
     addToBattleLog("New Turn Started");
     var playerAtk = player.charClass.baseAttack + player.charClass.bonusAttack;
     var playerDef =
@@ -308,11 +451,38 @@ $(document).ready(function () {
     var enemyDodge = enemy.charClass.baseDodge + enemy.charClass.bonusDodge;
     updateStatBlock(playerAtk, playerDef, playerSpd, playerDodge);
 
-    console.log("Battle start");
+    console.log("Battle start", currentStatuses);
+    resolveStatuses();
+    if (playerSpd >= enemySpd) {
+      setTimeout(function () {
+        player.charClass.moveset[playerAction].effect();
+      }, 2000 + 2000 * currentStatuses.length);
+      setTimeout(function () {
+        enemyTurn();
+      }, 4000 + 2000 * currentStatuses.length);
+    } else {
+      setTimeout(function () {
+        enemyTurn();
+      }, 2000 + 2000 * currentStatuses.length);
+      setTimeout(function () {
+        player.charClass.moveset[playerAction].effect();
+      }, 4000 + 2000 * currentStatuses.length);
+    }
+
     setTimeout(function () {
-      resolveStatuses();
-    }, 2000);
-    player.charClass.moveset[playerAction].effect();
+      enableButtons();
+      setBonusStatsZero(player);
+      playerAtk = player.charClass.baseAttack + player.charClass.bonusAttack;
+      playerDef = player.charClass.baseDefense + player.charClass.bonusDefense;
+      playerSpd = player.charClass.baseSpeed + player.charClass.bonusSpeed;
+      playerDodge = player.charClass.baseDodge + player.charClass.bonusDodge;
+      updateStatBlock(playerAtk, playerDef, playerSpd, playerDodge);
+      setBonusStatsZero(enemy);
+      enemyAtk = enemy.charClass.baseAttack + enemy.charClass.bonusAttack;
+      enemyDef = enemy.charClass.baseDefense + enemy.charClass.bonusDefense;
+      enemySpd = enemy.charClass.baseSpeed + enemy.charClass.bonusSpeed;
+      enemyDodge = enemy.charClass.baseDodge + enemy.charClass.bonusDodge;
+    }, 6000);
   }
 
   function resolveStatuses() {
@@ -322,17 +492,22 @@ $(document).ready(function () {
       var currentStatus = currentStatuses.pop();
       var statusName = currentStatus[0];
       var statusObject = statuses.find((status) => status.name === statusName);
-      statusObject.effect(currentStatus[2]);
+      setTimeout(function () {
+        statusObject.effect(currentStatus[2]);
+      }, 2000);
       currentStatus[1]--;
       if (currentStatus[1] > 0) {
         tempStatuses.push(currentStatus);
       }
     }
-    console.log(enemy.charClass.currentHP);
 
     while (tempStatuses.length > 0) {
       currentStatuses.push(tempStatuses.pop());
     }
+  }
+
+  function enemyTurn() {
+    console.log("Enemy turn");
   }
 
   ////////////////////////////
@@ -365,7 +540,59 @@ $(document).ready(function () {
     $("#enemyHP").html(enemy.charClass.currentHP);
     $("#enemyHPBar").attr("value", enemy.charClass.currentHP);
   }
-});
+
+  // Updates the stat block
+  function updateStatBlock(atk, def, spd, dodge) {
+    $("#playerAtkSt").html(atk);
+    $("#playerDefSt").html(def);
+    $("#playerSpdSt").html(spd);
+    $("#playerDodgeSt").html(dodge);
+  }
+
+  // Disables action buttons
+  function disableButtons() {
+    $("#attack").prop("disabled", true);
+    $("#defend").prop("disabled", true);
+    $("#heal").prop("disabled", true);
+    $("#special").prop("disabled", true);
+  }
+
+  // Enables action buttons
+  function enableButtons() {
+    $("#attack").prop("disabled", false);
+    $("#defend").prop("disabled", false);
+    $("#heal").prop("disabled", false);
+    $("#special").prop("disabled", false);
+  }
+
+  // Sets bonus stats to 0
+  function setBonusStatsZero(character) {
+    character.charClass.bonusAttack = 0;
+    character.charClass.bonusDefense = 0;
+    character.charClass.bonusSpeed = 0;
+    character.charClass.bonusDodge = 0;
+  }
+
+  // Generate a random number
+  function genRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Check if status effect is already applied
+  function checkStatus(character, status) {
+    for (var i = 0; i < currentStatuses.length; i++) {
+      if (
+        currentStatuses[i][0] === status &&
+        currentStatuses[i][2] === character
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  console.log("Battle Script Loaded");
+}); // End of document.ready
 
 /*
 Statuses
